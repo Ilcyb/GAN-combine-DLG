86049f0d8b1456553caecd12c32e81f34cfed939
ilcyb <hybmail1996@gmail.com>
2021-02-05 15:09:11 +0800

    windows下的兼容性

--- src/experiments.py
+++ src/experiments.py
@@ -6,7 +6,7 @@ import json
 
 def read_experiment_config(config_path):
     try:
-        with open(config_path, 'r') as f:
+        with open(config_path, 'r', encoding='utf-8') as f:
             return json.load(f)
     except FileNotFoundError:
         print('{} not exists'.format(config_path))
@@ -15,33 +15,36 @@ def read_experiment_config(config_path):
 config_files = []
 config_file_base_path = '../experiments_configs'
 
-for file in os.listdir(config_file_base_path):
-    new_config = os.path.join(config_file_base_path, file)
-    filename, file_ext = os.path.splitext(new_config)
-    if file_ext != '.json' or filename == '../experiments_configs/base_config':
-        continue
-    config_files.append(new_config)
-
 def execute_exp(config_file_path):
     config = read_experiment_config(config_file_path)
+    print(config)
     subprocess.call('python -u main.py --config-file={} --mode=production --base-config=../experiments_configs/base_config.json | tee ../logs/{}.log'.format(config_file_path, config['name']), shell=True)
 
-process_list = []
-for i in range(len(config_files)):
-    p = Process(target=execute_exp, args=(config_files[i], ))
-    p.start()
-    process_list.append(p)
+if __name__ == '__main__':
 
-print('共开启{}个进程'.format(len(process_list)))
-start_time = time.time()
+    for file in os.listdir(config_file_base_path):
+        new_config = os.path.join(config_file_base_path, file)
+        filename, file_ext = os.path.splitext(new_config)
+        if file_ext != '.json' or 'base_config' in filename:
+            continue
+        config_files.append(new_config)
 
-for p in process_list:
-    p.join()
+    process_list = []
+    for i in range(len(config_files)):
+        p = Process(target=execute_exp, args=(config_files[i], ))
+        p.start()
+        process_list.append(p)
 
-total_time = time.time() - start_time
-print(total_time)
-hours = int(total_time / 3600)
-mins = int((total_time - (hours*3600)) / 60)
-secs = int((total_time - (hours*3600) - (mins*60)))
+    print('共开启{}个进程'.format(len(process_list)))
+    start_time = time.time()
 
-print('所有进程执行完毕，共耗时{}小时{}分{}秒'.format(hours, mins, secs))
+    for p in process_list:
+        p.join()
+
+    total_time = time.time() - start_time
+    print(total_time)
+    hours = int(total_time / 3600)
+    mins = int((total_time - (hours*3600)) / 60)
+    secs = int((total_time - (hours*3600) - (mins*60)))
+
+    print('所有进程执行完毕，共耗时{}小时{}分{}秒'.format(hours, mins, secs))
